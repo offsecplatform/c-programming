6c506d65c867aa7032f9a85b9eda77efebb991dc
Offensive Security Platform <saide54532@gmail.com>
2025-12-08 20:21:37 +0600

    Create main.c

---
+++ exercise/triangle-pattern/main.c
@@ -0,0 +1,19 @@
+/*
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+*/
+#include <stdio.h>
+int main() {
+  int n;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      printf("%d  ", j);
+    }
+    printf("\n");
+  }
+  return 0;
+}
